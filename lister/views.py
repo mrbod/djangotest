64bb45b4f752ec3685aa95d37aad83a9c35e2995
Per Forser <per.forser@gmail.com>
2015-09-17 19:40:43 +0200

    listing works

--- lister/views.py
+++ lister/views.py
@@ -8,16 +8,26 @@ import os.path
 def index(request):
     #entry_list = Entry.objects.all()
     entry_list = []
-    parent = request.path
-    start = '.' + parent
-    for p, d, f in os.walk(start):
-        entry_list = [Entry(path=p) for p in d]
-        d[:] = []
+    base = request.path
+    if base.endswith('/'):
+        parent = ''
+    else:
+        if base.count('/') > 1:
+            parent = base[:base.rfind('/')]
+        else:
+            parent = '/'
+        base = base + '/'
+    for path, dirs, files in os.walk('.' + base):
+        entry_list = [Entry(path=d) for d in dirs if not d.startswith('.')]
+        break
+    print 'base', base
+    print 'parent', parent
     template = loader.get_template('lister/index.html')
     context = RequestContext(request,
             {
                 'entry_list': entry_list,
                 'parent': parent,
+                'base': base,
             }
         )
     return HttpResponse(template.render(context))
